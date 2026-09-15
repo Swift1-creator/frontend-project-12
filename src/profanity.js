e959eaa8cacd9c99fce7680f51a32560eafae978
Swift1-creator <kik.ss@yandex.ru>
2026-09-16 00:57:53 +0300

    .

--- src/profanity.js
+++ src/profanity.js
@@ -3,6 +3,11 @@ import leoProfanity from 'leo-profanity';
 leoProfanity.loadDictionary('en');
 leoProfanity.loadDictionary('ru');
 
+leoProfanity.add([
+  'boob',
+  'boobs',
+]);
+
 export const cleanText = (value) => (
   leoProfanity.clean(String(value ?? ''))
 );
