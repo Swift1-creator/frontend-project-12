9513d8f9af6639966831d86bac0e351ed63f6655
Swift1-creator <kik.ss@yandex.ru>
2026-09-16 00:26:46 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,19 @@ const ProtectedRoute = ({ children }) => {
 };
 
 const App = () => (
-  <>
-    <Notifications />
-
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route
-        path="/"
-        element={(
-          <ProtectedRoute>
-            <ChatPage />
-          </ProtectedRoute>
-        )}
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  </>
+  <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/signup" element={<SignupPage />} />
+    <Route
+      path="/"
+      element={(
+        <ProtectedRoute>
+          <ChatPage />
+        </ProtectedRoute>
+      )}
+    />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>
 );
 
 export default App;
