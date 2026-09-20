c8084d4497c499ec29ffd3f5e28921db2d60dc26
Swift1-creator <kik.ss@yandex.ru>
2026-09-21 00:26:23 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/frontend-project-12/',
+
   plugins: [react()],
 
   server: {
